d7cb2ff0f6aaa8cc9abfe44329632c8e9eda0248
Nick Swainston <nickaswainston@gmail.com>
2022-07-13 11:18:02 +0800

    Added the ability to search by observation ID on the candidates admin page

--- gleam_webapp/candidate_app/admin.py
+++ gleam_webapp/candidate_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Observation, Filter, Candidate, Rating
 
 class CandidateAdmin(admin.ModelAdmin):
+    search_help_text = "Filter by observation ID"
+    search_fields = ['obs_id__observation_id']
     list_filter = ('obs_id', )
     list_display = ('id', 'obs_id', 'filter')
     model = Candidate
