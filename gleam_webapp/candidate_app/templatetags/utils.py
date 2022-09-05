cfdc8e964b1836a3cb203b67c3b993945be0290e
Nick Swainston <nickaswainston@gmail.com>
2022-09-05 13:37:27 +0800

    Used kwargs and attribute magic to automatically loop over all candidate types in the candidate table view

--- gleam_webapp/candidate_app/templatetags/utils.py
+++ gleam_webapp/candidate_app/templatetags/utils.py
@@ -21,3 +21,11 @@ def url_replace(request, field, value, direction=''):
         dict_[field] = direction + value
 
     return urlencode(OrderedDict(sorted(dict_.items())))
+
+
+@register.simple_tag
+def get_type_count(dictionary, key):
+    # Taken from https://stackoverflow.com/questions/50703556/get-dictionary-value-by-key-in-django-template
+    # And edited for attributes
+    # Grab the count got the key type but changing the attributes to a dictionary
+    return dictionary.__dict__.get(key+"_count")
