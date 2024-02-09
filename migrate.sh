ccdb29823a4ac7a35b0e59c597079f92071ea2ff
PaulHancock <paul.hancock@curtin.edu.au>
2024-02-09 15:41:19 +0800

    create helper scripts

---
+++ migrate.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+docker-compose exec web python gleam_webapp/manage.py makemigrations
+docker-compose exec web python gleam_webapp/manage.py makemigrations candidate_app
+docker-compose exec web python gleam_webapp/manage.py migrate
