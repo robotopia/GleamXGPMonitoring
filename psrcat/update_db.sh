e342dfdfc7b3e645af5809395276922439fe5bc3
PaulHancock <paul.hancock@curtin.edu.au>
2024-02-14 10:19:40 +0800

    make a script to update the psr catalog

---
+++ psrcat/update_db.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# over writes existing file
+curl https://www.atnf.csiro.au/research/pulsar/psrcat/downloads/psrcat_pkg.tar.gz -o psrcat_pkg.tar.gz
+
+# overwrites existing db
+tar -xf psrcat_pkg.tar.gz ./psrcat_tar/psrcat.db 
+
+# note when we last updated the files
+echo "Updated db on $(date --utc)" >> update_db.log
