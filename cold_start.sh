395d1d7b638cf07debfdf43e30222f8cf84a05da
PaulHancock <paul.hancock@curtin.edu.au>
2024-02-14 10:50:35 +0800

    create a script for first time web app start

---
+++ cold_start.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# set up the psr databse
+cd psrcat
+./update_db.sh
+cd ..
+
+# build / start the web app
+docker-compose -f docker-compose.yml up --build -d
+
+# apply migrations and add an admin user
+./migrate.sh
+./make_admin.sh
+
+# install the q3c extension to our db
+./enable_q3c.sh
