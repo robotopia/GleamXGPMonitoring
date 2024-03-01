b3ceeea854a62b1588b3c9416bcf493cc0475162
PaulHancock <paul.hancock@curtin.edu.au>
2024-03-01 11:40:41 +0800

    add commands for loading fixtures

--- cold_start.sh
+++ cold_start.sh
@@ -14,3 +14,7 @@ docker-compose -f docker-compose.yml up --build -d
 
 # install the q3c extension to our db
 ./enable_q3c.sh
+
+# # load fixtures
+# docker-compose exec web python gleam_webapp/manage.py loaddata auth.json
+# docker-compose exec web python gleam_webapp/manage.py loaddata app.json
