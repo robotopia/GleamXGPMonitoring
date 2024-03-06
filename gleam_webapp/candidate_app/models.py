97a691b553bb2cd010a8a0e049e24a52a80898c8
PaulHancock <paul.hancock@curtin.edu.au>
2024-03-06 16:51:25 +0800

    create project model and link into candidate

--- gleam_webapp/candidate_app/models.py
+++ gleam_webapp/candidate_app/models.py
@@ -42,6 +42,16 @@ class Observation(models.Model):
         return f"{self.observation_id}"
 
 
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(
+        verbose_name="Project name", max_length=64, blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Filter(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(
@@ -68,6 +78,10 @@ class Candidate(models.Model):
         on_delete=models.CASCADE,
         related_name="candidate",
     )
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="candidate"
+    )
+
     png_path = models.FileField(upload_to="candidates/", max_length=1024, null=True)
     gif_path = models.FileField(upload_to="candidates/", max_length=1024, null=True)
     notes = models.TextField(blank=True, null=True, default="")
