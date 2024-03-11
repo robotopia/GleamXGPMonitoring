c4e797a3300209587e15609d0963cd069f0fc944
PaulHancock <paul.hancock@curtin.edu.au>
2024-03-11 09:00:49 +0000

    read allowed hosts from env

--- gleam_webapp/gleam_webapp/settings.py
+++ gleam_webapp/gleam_webapp/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "mwa-image-plane.duckdns.org"]
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
