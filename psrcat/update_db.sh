c0d8693bc6a680887a459f2858ba79f4d3cbc343
PaulHancock <paul.hancock@curtin.edu.au>
2024-03-13 06:34:48 +0000

    put db in the right place

--- psrcat/update_db.sh
+++ psrcat/update_db.sh
@@ -4,7 +4,7 @@
 curl https://www.atnf.csiro.au/research/pulsar/psrcat/downloads/psrcat_pkg.tar.gz -o psrcat_pkg.tar.gz
 
 # overwrites existing db
-tar -xvf psrcat_pkg.tar.gz psrcat_tar/psrcat.db --strip-components=1
+tar -xvf psrcat_pkg.tar.gz psrcat_tar/psrcat.db 
 
 # note when we last updated the files
 echo "Updated db on $(date --utc)" >> update_db.log
