f7b9a39be884299f5ea9f8a8035d48cab0af946f
PaulHancock <paul.hancock@curtin.edu.au>
2024-03-13 16:21:21 +0800

    create index along with q3c

--- enable_q3c.sh
+++ enable_q3c.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-docker compose exec db bash -c 'psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -c "CREATE EXTENSION q3c;"'
+docker compose exec db bash -c 'psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -c "CREATE EXTENSION q3c;CREATE INDEX ON candidate_app_candidate (q3c_ang2ipix(ra_deg, dec_deg));"'
