2441104f9176b35dbdeb021bc7d9e0fcfccf1e80
PaulHancock <paul.hancock@curtin.edu.au>
2024-05-22 10:50:52 +0800

    fix min/max filtering

--- gleam_webapp/candidate_app/filters.py
+++ gleam_webapp/candidate_app/filters.py
@@ -3,21 +3,21 @@ from .models import Candidate
 
 
 class CandidateFilter(django_filters.FilterSet):
-    id = django_filters.NumericRangeFilter(field_name="id", label="Candidate ID")
+    id = django_filters.RangeFilter(field_name="id", label="Candidate ID")
     can_det_stat = django_filters.NumericRangeFilter(
         field_name="can_det_stat", label="Detection Statistic"
     )
-    obs_id = django_filters.NumericRangeFilter(
+    obs_id = django_filters.RangeFilter(
         field_name="obs_id__observation_id",
         label="OBSID",
     )
-    ra_deg = django_filters.NumericRangeFilter(
-        field_name="ra_deg", label="RA (degrees)"
+    ra_deg = django_filters.RangeFilter(
+        field_name="ra_deg",
+        label="RA (degrees)",
+        widget=django_filters.widgets.RangeWidget(),
     )
-    dec_deg = django_filters.NumericRangeFilter(
-        field_name="dec_deg", label="DEC (degrees)"
-    )
-    can_fluence = django_filters.NumericRangeFilter(
+    dec_deg = django_filters.RangeFilter(field_name="dec_deg", label="DEC (degrees)")
+    can_fluence = django_filters.RangeFilter(
         field_name="can_fluence", label="Fluence (Jy.s)"
     )
 
