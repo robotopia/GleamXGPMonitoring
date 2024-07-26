d949f50a9241a58dd0e10d3f65969455b820a839
PaulHancock <paul.hancock@curtin.edu.au>
2024-07-26 16:50:27 +0800

    do cone search on candidate_table filters

--- gleam_webapp/candidate_app/filters.py
+++ gleam_webapp/candidate_app/filters.py
@@ -1,5 +1,11 @@
 import django_filters
+from django.db.models import Q, F
 from .models import Candidate
+from django_q3c.expressions import Q3CRadialQuery, Q3CDist
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class CandidateFilter(django_filters.FilterSet):
@@ -11,12 +17,20 @@ class CandidateFilter(django_filters.FilterSet):
         field_name="obs_id__observation_id",
         label="OBSID",
     )
-    ra_deg = django_filters.RangeFilter(
+    ra_deg = django_filters.NumberFilter(
         field_name="ra_deg",
         label="RA (degrees)",
-        widget=django_filters.widgets.RangeWidget(),
+        method="no_filter",
     )
-    dec_deg = django_filters.RangeFilter(field_name="dec_deg", label="DEC (degrees)")
+    dec_deg = django_filters.NumberFilter(
+        field_name="dec_deg",
+        label="DEC (degrees)",
+        method="no_filter",
+    )
+    radius = django_filters.NumberFilter(
+        label="radius (arcmin)", method="filter_by_cone_search"
+    )
+
     can_fluence = django_filters.RangeFilter(
         field_name="can_fluence", label="Fluence (Jy.s)"
     )
@@ -24,3 +38,48 @@ class CandidateFilter(django_filters.FilterSet):
     class Meta:
         model = Candidate
         fields = []
+
+    def no_filter(self, qs, name, value):
+        """
+        Over-ride whatever filtering would have taken place
+        """
+        return qs
+
+    def filter_by_cone_search(self, queryset, name, value):
+        logger.warning(f"QS size is {queryset.count()}")
+
+        if not value:
+            return queryset
+
+        ra_center = self.data.get("ra_deg")
+        dec_center = self.data.get("dec_deg")
+
+        if (len(ra_center) == 0) or (len(dec_center) == 0):
+            return queryset
+        ra_center = float(ra_center)
+        dec_center = float(dec_center)
+        radius = float(value) / 60.0  # arcmin
+
+        logger.warning(f"ra {ra_center}, dec {dec_center}, radius {radius}")
+        # Use Q3C spatial query for the cone search
+        qs = queryset.filter(
+            Q(
+                Q3CRadialQuery(
+                    center_ra=ra_center,
+                    center_dec=dec_center,
+                    ra_col="ra_deg",
+                    dec_col="dec_deg",
+                    radius=radius,
+                )
+            )
+        )
+        qs = qs.annotate(  # do the distance calcs in the db
+            sep=Q3CDist(
+                ra1=F("ra_deg"),
+                dec1=F("dec_deg"),
+                ra2=ra_center,
+                dec2=dec_center,
+            )
+            * 60  # deg -> arcmin
+        ).order_by("sep")
+        return qs
