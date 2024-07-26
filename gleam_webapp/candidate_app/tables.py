0dcdab022a5a6961fe8e2d6d98ed08e3ce1be390
PaulHancock <paul.hancock@curtin.edu.au>
2024-07-26 16:57:50 +0800

    disable sorting based on preview/metadata

--- gleam_webapp/candidate_app/tables.py
+++ gleam_webapp/candidate_app/tables.py
@@ -10,7 +10,7 @@ class ImageColumn(tables.Column):
 
 class CandidateTable(tables.Table):
     rating_count = tables.Column()
-    png_path = ImageColumn(verbose_name="Preview")
+    png_path = ImageColumn(verbose_name="Preview", orderable=False)
     ra_hms = tables.Column(order_by=("ra_deg",), verbose_name="RA")
     dec_dms = tables.Column(order_by=("dec_deg",), verbose_name="DEC")
     obs_id = tables.Column(verbose_name="OBSID")
@@ -21,6 +21,7 @@ class CandidateTable(tables.Table):
         text=lambda record: "View Meta" if hasattr(record, "metadata") else "",
         args=[tables.A("pk")],
         verbose_name="Metadata",
+        orderable=False,
     )
 
     sep = tables.Column(verbose_name="Sep")
